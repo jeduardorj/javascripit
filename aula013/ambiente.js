d706708e12ef01fc56b2e48969f27d87c7090c2f
eduoliveirarj <104588175+jeduardorj@users.noreply.github.com>
2022-07-12 13:29:55 -0300

    criei a pasta aula013 e o arquivo ambiente .js

---
+++ aula013/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1(c recenbe c mais 1)
+}while (c <= 3)
+
+
+/*while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1(c recenbe c mais 1)
+} */
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
